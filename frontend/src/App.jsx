2625476c7860d7343ab8814b315adac04c2960cb
GaneshGorana <ganeshgorana01@gmail.com>
2024-05-05 22:31:31 +0530

    changed turn server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,9 @@ function App() {
           urls: "stun:stun1.l.google.com:19302",
         },
         {
-          urls: "stun:stun2.l.google.com:19302",
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credentials: "openrelayproject",
         },
       ],
     }),
